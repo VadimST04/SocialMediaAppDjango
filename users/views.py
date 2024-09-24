165c176858358308e4d70dd00449ea9c302492fe
VadimST04 <vadikstarchak04@gmail.com>
2024-09-24 21:48:34 +0200

    add and process login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from users.forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password']
+            )
+            if user:
+                login(request, user)
+                return HttpResponse('user is authenticated and logged in')
+            else:
+                return HttpResponse('Invalid Credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
